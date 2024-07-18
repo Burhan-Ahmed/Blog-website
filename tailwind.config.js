ee704b0a9e451d72e3e86afd6e16be16fb88d659
Burhan <burhan.ahmed60090@gmail.com>
2024-07-18 19:59:06 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        customPurple: '#110729',  // Define your custom colors here
+      },
+    },
+  },
+  plugins: [],
+}
+
