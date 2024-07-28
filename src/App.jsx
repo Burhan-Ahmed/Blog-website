e9c4c9d9b58fc1f7cffd7d52fa0f0740ede9774d
Burhan <burhan.ahmed60090@gmail.com>
2024-07-28 11:52:19 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,60 @@
 import './App.css'
-import React from 'react';
-
+import React, { useState } from 'react';
 
 export default function App() {
+  const [color, setColor] = useState('');
+  const [getcolor, setNewColor] = useState('');
+
+  const ChangeColor = (event) => {
+    setNewColor(event.target.value)
+  }
+
+  const Paint = (event) => {
+    event.preventDefault();
+    setColor(getcolor)
+  }
 
   return (
     <>
       <div className='text-center m-12 text-4xl font-semibold'>
         <h1>  Color Picker </h1>
       </div>
-      <form className='my-24 mx-96 flex justify-center'>
-        <div className='space-y-10 border rounded-lg border-black py-14 px-20 shadow-lg'>
-          <div className='text-center border-b'>
+      <form className='mx-auto flex justify-center'>
+        <div style={{ backgroundColor: color }} className='space-y-5 border rounded-lg border-black py-14 px-20 shadow-lg'>
+          <div className='text-center text-xl  border-b'>
             <h1>Billing details</h1>
           </div>
-          <div className='border border-black w-fit flex space-x-2'>
-            <input className='border border-black ps-5 py-0.5 rounded-md' type="text" placeholder='First Name' />
-            <input className='border border-black ps-5 py-0.5 rounded-md' type="text" placeholder='Last Name' />
+          <div className='flex space-x-5'>
+            <div class="relative z-0 w-full mb-6 group">
+              <input type="text" name="floating_name" id="floating_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder='' />
+              <label for="floating_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First Name</label>
+            </div>
+            <div class="relative z-0 w-full mb-6 group">
+              <input type="text" name="floating_name" id="floating_name" class="block py-2.5 px-0 w-full text-sm  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder='' />
+              <label for="floating_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last Name</label>
+            </div>
           </div>
-          <div className='flex flex-col border border-black space-y-2'>
-            <input className='border border-black ps-5 py-0.5 rounded-md' type='address' placeholder='Address' />
-            <input className='border border-black ps-5 py-0.5 rounded-md' type='email' placeholder='Email' />
-            <input className='border border-black ps-5 py-0.5 rounded-md' type='number' placeholder='Phone Number' />
-            <label htmlFor="colour">Color</label>
-            <input className='border w-full border-black ps-5 py-0.5 rounded-md' name='colour' type='color' placeholder='red' />
+          <div>
+            <div class="relative z-0 w-full mb-6 group">
+              <input type="address" name="floating_name" id="floating_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder='' />
+              <label for="floating_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Address</label>
+            </div>
+            <div class="relative z-0 w-full mb-6 group">
+              <input type="email" name="floating_name" id="floating_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder='' />
+              <label for="floating_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
+            </div>
+            <div class="relative z-0 w-full mb-6 group">
+              <input type="number" name="floating_name" id="floating_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder='' />
+              <label for="floating_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone</label>
+            </div>
+            <div>
+              <label htmlFor="colour">Color</label>
+              <input value={getcolor} onChange={ChangeColor} className='border w-full border-black ps-5 py-0.5 rounded-md' name='colour' type='color' />
+            </div>
           </div>
           <div className='flex justify-center'>
-            <button className=' border border-black px-6 py-1 rounded-lg font-semibold'>
+            <button onClick={Paint}
+              className='bg-white border border-black px-6 py-1 rounded-lg font-semibold'>
               CHANGE COLOR
             </button>
           </div>
