bd9416ff6131c20a728a3a27b64805ce6c2b962e
Burhan <burhan.ahmed60090@gmail.com>
2024-08-05 05:12:21 +0500

    Initial commit

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,4 +1,5 @@
 import NavBar from "./headings/NavBar"
+import Footer from "./headings/footer";
 
 export default function ContactPage() {
     return (
@@ -43,52 +44,7 @@ export default function ContactPage() {
                     </form>
                 </div>
             </body>
-
-            <footer class="bg-cyan-600">
-                <div class="flex justify-around py-7">
-                    <div>
-                        <h1 class="flex justify-center text-xl font-semibold">
-                            E-COMMERCE STORE
-                        </h1>
-                        <p>
-                            At Our Ecommerce Store, we are passionate about providing exceptional products and an <br />
-                            unparalleled
-
-                            shopping experience. We strive to offer a curated selection of high-quality <br />
-                            items that meet the
-                            diverse
-                            needs and tastes of our valued customers.
-                        </p>
-                    </div>
-                    <div>
-                        <h1 class="font-semibold">
-                            LINKS
-                        </h1>
-                        <ul>
-                            <li>Home</li>
-                            <li> Products</li>
-                            <li> Cart</li>
-                            <li> Contact Us</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h1 class="font-semibold">
-                            LINKS
-                        </h1>
-                        <ul>
-                            <li>Facebook</li>
-                            <li> Instagram</li>
-                            <li> Twitter</li>
-                            <li> LinkedIn</li>
-                        </ul>
-                    </div>
-                </div>
-                <div>
-                    <h5 class="flex justify-center">
-                        © 2020 Copyright: ecommerce-store.com
-                    </h5>
-                </div>
-            </footer>
+            <Footer />
         </>
     )
 }
